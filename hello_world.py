ced2313f4966d9a304c205fb3f822f238355b107
Katzumi Urdininea <katurdininea02@gmail.com>
2024-11-25 08:31:24 -0400

    amigo

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     nombre = os.getenv("USERNAME")
-    print(f"¡Hola, {nombre} desde GitHub!")
+    print(f"¡Hola, {nombre} desde GitHub, tequiero :)!")
 
 if __name__ == "__main__":
     main()
